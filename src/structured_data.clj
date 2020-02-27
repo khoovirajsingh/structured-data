e410110962bd35ba4c64703d2d9b854f692565eb
khoovis <khoovirajsingh.seewoonauth@mkodo.com>
2020-02-27 20:33:32 +0000

    Exercise 5 done

--- src/structured_data.clj
+++ src/structured_data.clj
@@ -26,11 +26,15 @@
 (defn rectangle [bottom-left top-right]
   [bottom-left top-right])
 
-(defn width [rectangle]
-  :-)
+(defn width 
+  [rectangle]
+  (let [[[x1 y1] [x2 y2]] rectangle]
+    (- x2 x1)))
+    
 
 (defn height [rectangle]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle]
+    (- y2 y1)))
 
 (defn square? [rectangle]
   :-)
